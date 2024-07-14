227a99c767839f513363c11c8c83ef34b0966b46
Annacletter <anna.cletter12@gmail.com>
2024-07-13 23:54:49 -0700

    Added Backend directory without embedded Git repository

--- server/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createBooking, getBookings } = require('../controllers/bookingController');
-const auth = require('../middleware/auth');
-const router = express.Router();
-
-router.post('/', auth, createBooking);
-router.get('/', auth, getBookings);
-
-module.exports = router;
