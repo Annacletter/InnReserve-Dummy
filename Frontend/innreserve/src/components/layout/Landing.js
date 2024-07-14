e541f4ce0e58a80b0db42d8129ea512a8e4da248
Annacletter <anna.cletter12@gmail.com>
2024-07-14 02:05:31 -0700

    corrections

--- Frontend/innreserve/src/components/layout/Landing.js
+++ Frontend/innreserve/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import App from './app';
 import { Link } from 'react-router-dom';
 
 const Landing = () => {
--- Frontend/innreserve/src/components/layout/Navbar.js
+++ Frontend/innreserve/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import App from './app';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
 
