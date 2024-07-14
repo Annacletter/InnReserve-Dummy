dfb0788362640d50b9c804d2505b89261fa99382
Annacletter <anna.cletter12@gmail.com>
2024-07-14 15:39:41 -0700

    Amending the Landing

--- Frontend/innreserve/src/components/layout/Landing.js
+++ Frontend/innreserve/src/components/layout/Landing.js
@@ -14,13 +14,16 @@ const Landing = () => {
           </div>
         </div>
       </div>
+      <div>About</div>
+      <div>
+        <p className="small">
+          The InnReserve Booking System is an easy-to-use web application designed to make booking restaurants and hotels simple and efficient. It helps customers quickly secure their dining and lodging experiences while providing businesses with a handy tool to manage and improve their reservations.
+          By offering a seamless reservation process, the InnReserve Booking System aims to enhance overall customer satisfaction. Additionally, it equips businesses with the tools they need to effectively manage and grow their operations.
+        </p>
+      </div>
     </div>
-	  <div> About </div>
-	  <div>
-	  <p className="small">The InnReserve Booking System is an easy-to-use web application designed to make booking restaurants and hotels simple and efficient. It helps customers quickly secure their dining and lodging experiences while providing businesses with a handy tool to manage and improve their reservations.
-By offering a seamless reservation process, the InnReserve Booking System aims to enhance overall customer satisfaction. Additionally, it equips businesses with the tools they need to effectively manage and grow their operations</p>
-	  <div>
   );
 };
 
 export default Landing;
+
