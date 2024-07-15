84e031fb58df1babb1f97c9898d7a7b145e3ad10
Annacletter <anna.cletter12@gmail.com>
2024-07-15 09:57:26 -0700

    Modifying the Navbar

--- Frontend/innreserve/src/components/layout/Navbar.js
+++ Frontend/innreserve/src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
   const authContext = useContext(AuthContext);
@@ -35,11 +36,33 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
-        <Link to="/" className="navbar-brand">InnReserve</Link>
+        <Link to="/" className="navbar-brand">InnReserve Booking System</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link to="/" className="nav-link">
+                Home Page <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/restaurants-hotels" className="nav-link">
+                Restaurants/Hotels
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/price-range" className="nav-link">
+                Price range
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/disabled" className="nav-link disabled">
+                Disabled
+              </Link>
+            </li>
+          </ul>
           <ul className="navbar-nav ml-auto">
             {isAuthenticated ? authLinks : guestLinks}
           </ul>
